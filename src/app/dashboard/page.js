6215c951230ea858f704928e29c44118468b9860
otemure <otemure.george@yahoo.com>
2024-12-18 12:36:32 +0100

    renamed files

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,4 +1,4 @@
-import Sidebar from "../components/Sidebar";
+import Sidebar from "../components/Side";
 
 export default function Dashboard() {
   return (
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import localFont from "next/font/local";
 import "./globals.css";
-import Sidebar from "./components/Sidebar";
+import Sidebar from "./components/Side";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
